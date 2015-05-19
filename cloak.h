d97e88890f9e462e5ae47471c633be0820b87cf8
Paul <pfultz2@yahoo.com>
2015-05-19 11:41:04 -0500

    Add a license

--- cloak.h
+++ cloak.h
@@ -1,4 +1,9 @@
-
+/*=============================================================================
+    Copyright (c) 2015 Paul Fultz II
+    cloak.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
 
 #define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
 #define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
